5578d2bc27201db27a201e69dc2ab95c44feda26
miujik <miujik@163.com>
2018-09-02 00:24:37 +0800

    branch test

--- Hello.java
+++ Hello.java
@@ -3,6 +3,7 @@ public class Hello
     public static void main(String[] args)
     {
         System.out.println("Hello, java");
+        System.out.println("Test branch");
         // add something
         // push test
     }
