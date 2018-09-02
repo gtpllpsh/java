be12a152607f9dff88b76cf5826dba12092caf9b
miujik <miujik@163.com>
2018-09-02 09:21:46 +0800

    add se dir

---
+++ se/Hello.java
@@ -0,0 +1,12 @@
+public class Hello
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, java");
+        System.out.println("Test branch");
+        // push test
+        // conflict?
+        // bug fix
+        // stash test
+    }
+}
